bb711f1999be2d98e436cfc4bf0e5563ad257535
Andre Luiz <andresanteria@hotmail.com>
2025-12-10 16:46:08 -0300

    mscard project creation

---
+++ mscard/src/main/java/io/github/cursomservice/mscard/MscardApplication.java
@@ -0,0 +1,13 @@
+package io.github.cursomservice.mscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscardApplication.class, args);
+	}
+
+}
