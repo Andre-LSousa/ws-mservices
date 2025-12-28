040d2abf76c3c1746d8750140748f19d70a8aced
Andre Luiz <andresanteria@hotmail.com>
2025-12-28 17:40:27 -0300

    add OpenAPI documentation with Swagger UI

--- mscard/src/main/java/io/github/cursomservice/mscard/MscardApplication.java
+++ mscard/src/main/java/io/github/cursomservice/mscard/MscardApplication.java
@@ -1,5 +1,6 @@
 package io.github.cursomservice.mscard;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +9,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableRabbit
+@Slf4j
 public class MscardApplication {
 
 	public static void main(String[] args) {
+		log.info("informacao: {}", "teste info");
+		log.error("error: {}", "teste error");
+		log.warn("aviso: {}", "teste aviso");
 		SpringApplication.run(MscardApplication.class, args);
 	}
 
--- mscard/src/main/java/io/github/cursomservice/mscard/infra/mqueue/CardIssuanceSubscriber.java
+++ mscard/src/main/java/io/github/cursomservice/mscard/infra/mqueue/CardIssuanceSubscriber.java
@@ -8,12 +8,14 @@ import io.github.cursomservice.mscard.domain.CardIssuanceRequestData;
 import io.github.cursomservice.mscard.infra.repository.CardClientRepository;
 import io.github.cursomservice.mscard.infra.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CardIssuanceSubscriber {
 
     private final CardRepository cardRepository;
@@ -35,7 +37,7 @@ public class CardIssuanceSubscriber {
             cardClientRepository.save(cardClient);
 
         }catch(JsonProcessingException e){
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissao de cartao: {} ", e.getMessage());
         }
     }
 }
